3bb30fe00670af996164ae89b20707590c108bd5
Sebastián Iñones <sebainones@gmail.com>
2020-05-19 10:53:44 +0200

    Add project files.

---
+++ NoSqlApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoSqlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
